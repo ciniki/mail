621ad2701f24e7728a5729d3f0e072d0aa4b63b0
Andrew Rivett <veggiefrog@gmail.com>
2013-09-03 20:16:18 -0400

    Fixed colours in default mail theme

--- private/themeDefault.php
+++ private/themeDefault.php
@@ -20,9 +20,9 @@ function ciniki_mail_themeDefault($ciniki, $business_id) {
 			. "a:hover {text-decoration: none;}\n"
 			. "a:active {text-decoration: none;}\n"
 			. "",
-		'wrapper_style'=>"background-color: #000;",
-		'title_style'=>'font-size: 16px Helvetica, sans-serif; line-height: 20px; color: #aaffff;',
-		'subtitle_style'=>'font-size: 14px Helvetica, sans-serif; line-height: 28px; color: #aaffff;',
+		'wrapper_style'=>"background-color: #FFF;",
+		'title_style'=>'font-size: 16px Helvetica, sans-serif; line-height: 20px; color: #439BBD;',
+		'subtitle_style'=>'font-size: 14px Helvetica, sans-serif; line-height: 28px; color: #439BBD;',
 		'logo_style'=>'',
 		'p'=>'font-size: 12px Helvetica, sans-serif; line-height: 16px; color: #969696; text-align: left;',
 		'h1'=>'',
