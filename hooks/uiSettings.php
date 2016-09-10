0fa7cfef1a49216f52b780065a9b718b3dfcae83
Andrew Rivett <veggiefrog@gmail.com>
2016-09-10 11:42:08 -0400

    Added hooks/uiSettings for new business menu

--- hooks/uiSettings.php
+++ hooks/uiSettings.php
@@ -7,14 +7,17 @@
 // Arguments
 // ---------
 // ciniki:
-// business_id:     The ID of the business to get events for.
+// business_id:     The ID of the business to get mail for.
 //
 // Returns
 // -------
 //
 function ciniki_mail_hooks_uiSettings($ciniki, $business_id, $args) {
 
-    $settings = array();
+    //
+    // Setup the default response
+    //
+    $rsp = array('stat'=>'ok', 'menu_items'=>array());
 
     //
     // Get the settings
@@ -22,9 +25,27 @@ function ciniki_mail_hooks_uiSettings($ciniki, $business_id, $args) {
     $rc = ciniki_core_dbDetailsQueryDash($ciniki, 'ciniki_mail_settings', 'business_id', 
         $business_id, 'ciniki.mail', 'settings', '');
     if( $rc['stat'] == 'ok' && isset($rc['settings']) ) {
-        $settings = $rc['settings'];
+        $rsp['settings'] = $rc['settings'];
     }
 
-    return array('stat'=>'ok', 'settings'=>$settings);  
+    //
+    // Check permissions for what menu items should be available
+    //
+    if( isset($ciniki['business']['modules']['ciniki.mail'])
+        && (isset($args['permissions']['owners'])
+            || isset($args['permissions']['employees'])
+            || isset($args['permissions']['resellers'])
+            || ($ciniki['session']['user']['perms']&0x01) == 0x01
+            )
+        ) {
+        $menu_item = array(
+            'priority'=>2000,
+            'label'=>'Mail', 
+            'edit'=>array('app'=>'ciniki.mail.main'),
+            );
+        $rsp['menu_items'][] = $menu_item;
+    } 
+
+    return $rsp;
 }
 ?>
